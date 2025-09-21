71fdbf16d1087c6c1750fedaaf8a9afe911a2137
Sam Nie <nie.sa@northeastern.edu>
2025-09-20 22:47:14 -0400

    Created infrastructure with Terraform

---
+++ infrastructure/main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "rds" {
+  source  = "terraform-aws-modules/rds/aws"
+  version = "6.12.0"
+  identifier = "ray-db"
+
+  engine            = "mysql"
+  engine_version    = "5.7"
+  instance_class    = "db.t3.micro"
+
+  allocated_storage = 5
+
+  db_name  = "raydb"
+  username = "user"
+  port     = "3306"
+
+  iam_database_authentication_enabled = true
+
+  create_db_option_group = false
+  create_db_parameter_group = false
+  auto_minor_version_upgrade = false
+}
