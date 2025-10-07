ae897cdc97e4319d48eba75df935ae4d9210e7a5
klytne <tienganekelly@gmail.com>
2025-10-07 18:31:33 +0200

    Remove login and register buttons

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,10 +3,6 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   return (
     <div>
-      <div className="flex gap-4">
-        <Link to="/login">Login</Link>
-        <Link to="/register">Register</Link>
-      </div>
       {/* TODO: Add landing page content */}
     </div>
   );
